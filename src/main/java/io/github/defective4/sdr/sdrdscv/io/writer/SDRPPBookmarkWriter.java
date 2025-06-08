956d65683f6463c5fddabb36910ee6200f41e831
Defective <def3ctive4@gmail.com>
2025-06-08 20:09:18 +0200

    Make SDR++ convert tags into lists

--- src/main/java/io/github/defective4/sdr/sdrdscv/io/writer/SDRPPBookmarkWriter.java
+++ src/main/java/io/github/defective4/sdr/sdrdscv/io/writer/SDRPPBookmarkWriter.java
@@ -1,7 +1,10 @@
 package io.github.defective4.sdr.sdrdscv.io.writer;
 
 import java.io.Writer;
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
@@ -12,13 +15,16 @@ import io.github.defective4.sdr.sdrdscv.radio.RadioStation;
 
 public class SDRPPBookmarkWriter implements BookmarkWriter {
 
+    private final String defaultListName;
     private final Gson gson;
-    private final String listName;
+    private final boolean ignoreTags;
 
     public SDRPPBookmarkWriter(
-            @ConstructorParam(argName = "list-name", defaultValue = "Discovered", description = "Sets name of the list containing detected stations.") String listName,
-            @ConstructorParam(argName = "disable-pretty-print", defaultValue = "true", description = "Disables pretty printing of the resulting JSON, saving space.") boolean prettyPrint) {
-        this.listName = listName;
+            @ConstructorParam(argName = "default-list-name", defaultValue = "Discovered", description = "Sets name of the list for untagged stations.") String listName,
+            @ConstructorParam(argName = "disable-pretty-print", defaultValue = "true", description = "Disables pretty printing of the resulting JSON, saving space.") boolean prettyPrint,
+            @ConstructorParam(argName = "ignore-tags", defaultValue = "false", description = "Ignores station tags assigned by other services and adds all stations in the default list.") boolean ignoreTags) {
+        defaultListName = listName;
+        this.ignoreTags = ignoreTags;
         GsonBuilder builder = new GsonBuilder();
         if (prettyPrint) builder = builder.setPrettyPrinting();
         gson = builder.create();
@@ -28,8 +34,9 @@ public class SDRPPBookmarkWriter implements BookmarkWriter {
     public void write(Writer output, List<RadioStation> stations) {
         JsonObject root = new JsonObject();
         JsonObject lists = new JsonObject();
-        JsonObject list = new JsonObject();
+        JsonObject defaultlist = new JsonObject();
         JsonObject bookmarks = new JsonObject();
+        Map<String, JsonObject> tagBookmarks = new LinkedHashMap<>();
 
         for (RadioStation station : stations) {
             JsonObject bookmark = new JsonObject();
@@ -41,15 +48,28 @@ public class SDRPPBookmarkWriter implements BookmarkWriter {
             bookmark.addProperty("frequency", station.getFrequency());
             bookmark.addProperty("mode", station.getModulation().getSdrppMod());
             bookmarks.add(station.getName(), bookmark);
+
+            if (!ignoreTags && station.getMetadata().containsKey(RadioStation.METADATA_TAGS)) {
+                String[] tags = station.getMetadataValue(RadioStation.METADATA_TAGS, String.class, "").split(",");
+                for (String tag : tags) if (!tag.isBlank() && !tag.equals(defaultlist)) {
+                    tagBookmarks.computeIfAbsent(tag, e -> new JsonObject()).add(station.getName(), bookmark);
+                }
+            }
         }
 
-        list.addProperty("showOnWaterfall", true);
-        list.add("bookmarks", bookmarks);
+        defaultlist.addProperty("showOnWaterfall", true);
+        defaultlist.add("bookmarks", bookmarks);
 
-        lists.add(listName, list);
+        lists.add(defaultListName, defaultlist);
+        for (Entry<String, JsonObject> entry : tagBookmarks.entrySet()) {
+            JsonObject list = new JsonObject();
+            list.addProperty("showOnWaterfall", true);
+            list.add("bookmarks", entry.getValue());
+            lists.add(entry.getKey(), list);
+        }
 
         root.addProperty("bookmarkDisplayMode", 1);
-        root.addProperty("selectedList", listName);
+        root.addProperty("selectedList", defaultListName);
         root.add("lists", lists);
 
         gson.toJson(root, output);
